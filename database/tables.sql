da04dbac0039d0df1c253ac46990c9f7cdd95bae
m031506 <dudakovic.halil@mayo.edu>
2022-05-11 17:25:45 -0500

    improve spacing

--- database/tables.sql
+++ database/tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE accident (
-	casenum VARCHAR(20)
-	--TODO add interesting features
+    casenum VARCHAR(20)
+    --TODO add interesting features
 );
 
 CREATE TABLE vehicle (
-	casenum VARCHAR(20) NOT NULL,
-	veh_no INTEGER NOT NULL
-	--TODO add interesting features
+    casenum VARCHAR(20) NOT NULL,
+    veh_no INTEGER NOT NULL
+    --TODO add interesting features
 );
 
 CREATE TABLE person (
-	casenum VARCHAR(20) NOT NULL,
-	per_no INTEGER NOT NULL,
-	veh_no INTEGER NOT NULL
-	--TODO add interesting features
+    casenum VARCHAR(20) NOT NULL,
+    per_no INTEGER NOT NULL,
+    veh_no INTEGER NOT NULL
+    --TODO add interesting features
 );
